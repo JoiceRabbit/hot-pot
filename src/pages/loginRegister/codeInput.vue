79c1e8e9b998559c12fdfa51b6c089cf704e2651
L-cw <goodlcw1@163.com>
2018-01-11 22:52:33 +0800

    18-01-11 divide component

---
+++ src/pages/loginRegister/codeInput.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <input class="message-input" type="text" placeholder="验证码">
+    <div class="send-code" 
+          :class="{'code-active': codeShow}"
+          @click="handleSendCode">
+        发送验证码</div>
+  </div>
+</template>
+
+<script>
+  // import axios from 'axios'
+  export default {
+    name: 'CodeInput',
+    props: {
+      codeShow: Boolean
+    },
+    methods: {
+      handleSendCode () {
+        if (this.codeShow) {
+          this.$emit('send')
+          //  此处应弹出提示提醒用户手机号不合法
+        }
+      },
+      handleSendCodeSucc (res) {
+        console.log(res.data.ret)
+      },
+      handleSendCodeErr (e) {
+        console.log(2)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+  @import '../../assets/styles/common/varibles.styl'
+  .message-input
+    width: 100%
+    line-height: .8rem
+    font-size: .3rem
+    text-indent: .2rem
+    color: $lightFont
+    background: $loginInputBg
+  .send-code
+    position: absolute
+    top: .48rem
+    right: .14rem
+    width: 1.6rem
+    height: .54rem
+    line-height: .54rem
+    text-align: center
+    font-size: .26rem
+    color: #fff
+    background: #b0d5a2
+  .code-active
+    background: #4cd96f
+</style>
--- src/pages/loginRegister/messageLogin.vue
+++ src/pages/loginRegister/messageLogin.vue
@@ -1,44 +1,67 @@
 <template>
   <div class="message-login">
     <div class="message-input-con">
-      <input class="message-input" v-model="phoneNum" type="text" placeholder="手机号" @input="handlePhoneNum">
+      <phone-input @codeShow="handleCodeShow"
+                   ref="phone"></phone-input>
     </div>
     <div class="message-input-con">
-      <input class="message-input" type="text" placeholder="验证码">
-      <div class="send-code" 
-            :class="{'code-active': codeShow}"
-            @click="handleSendCode">
-          发送验证码</div>
+      <code-input :codeShow="codeShow"
+                  @send="handleSendCode"></code-input>
     </div>
     <p class="notice">温馨提示：未注册同城火锅帐号的手机号，登录时将自动注册，且代表您已同意<span>《用户服务协议》</span></p>
     <div class="login-btn">登录</div>
+    <tool-tip :errMessage="errMessage"
+              v-show="errMessageShow"
+              @miss="handleErrMiss"></tool-tip>
   </div>
 </template>
 
 <script>
+  import ToolTip from 'components/ui/toolTip.vue'
+  import PhoneInput from './phoneInput.vue'
+  import CodeInput from './codeInput.vue'
+  import axios from 'axios'
   export default {
     name: 'messageLogin',
     data () {
       return {
         phoneNum: '',
-        codeShow: false
+        codeShow: false,
+        errMessage: '',
+        errMessageShow: false
       }
     },
+    components: {
+      PhoneInput,
+      CodeInput,
+      ToolTip
+    },
     methods: {
-      handlePhoneNum (e) {
-        this.phoneNum = e.target.value
-        if (this.phoneNum.length >= 11) {
-          this.phoneNum = this.phoneNum.slice(0, 11)
-          this.codeShow = true
+      handleErrMiss () {
+        this.errMessageShow = false
+        this.errMessage = ''
+      },
+      handleCodeShow (e) {
+        this.codeShow = e.codeShow
+      },
+      handleSendCode () {
+        if (this.$refs.phone.checkPhoneNum()) {
+          axios.post('/api/user/getVerCode', {
+            tel: this.phoneNum
+          })
+          .then(this.handleSendCodeSucc.bind(this))
+          .catch(this.handleSendCodeErr.bind(this))
         } else {
+          this.errMessage = '请输入正确手机号'
+          this.errMessageShow = true
           this.codeShow = false
         }
       },
-      handleSendCode () {
-        console.log(this.checkPhoneNum())
+      handleSendCodeSucc (res) {
+        console.log(res.data.ret)
       },
-      checkPhoneNum () {
-        return /^1[3578]\d{9}$/.test(this.phoneNum)
+      handleSendCodeErr (err) {
+        console.log(err)
       }
     }
   }
@@ -50,26 +73,6 @@
     position: relative
     width: 100%
     padding-top: .35rem
-    .message-input
-      width: 100%
-      line-height: .8rem
-      font-size: .3rem
-      text-indent: .2rem
-      color: $lightFont
-      background: $loginInputBg
-    .send-code
-      position: absolute
-      top: .48rem
-      right: .14rem
-      width: 1.6rem
-      height: .54rem
-      line-height: .54rem
-      text-align: center
-      font-size: .26rem
-      color: #fff
-      background: #b0d5a2
-    .code-active
-      background: #4cd96f
   .notice
     padding-top: .35rem
     font-size: .12rem
