30d8d024d11f97f2be0d214d9112c04fb5012fe0
L-cw <goodlcw1@163.com>
2018-01-15 20:30:37 +0800

    add

--- src/pages/shop/index.vue
+++ src/pages/shop/index.vue
@@ -29,6 +29,11 @@
                  :shopOrderInfo="shopOrderInfo"
                  :shopIntroInfo="shopIntroInfo"></component>
     </div>
+    <div class="cart">
+      <div class="cart-car"></div>
+      <div class="current-con"></div>
+      <div class="count">结算</div>
+    </div>
   </div>
 </template>
 
@@ -149,10 +154,15 @@
       .logo-img
         width: 100%
   .tab
+    position: sticky
+    left: 0
+    top: 0
+    z-index: 2
     display: flex
     line-height: .72rem
     font-size: .26rem
     color: #333
+    background: #fff
     .tab-item-con
       width: 33%
       padding: 0 .9rem
@@ -163,4 +173,30 @@
         text-align: center
       .tab-active
         background: url(../../../static/images/shop-tab-bg.png) bottom repeat-X
+  .cart
+    position: fixed
+    width: 100%
+    left: 0
+    bottom: 0
+    height: 1.34rem
+    background: rgba(0, 0, 0, .5)
+    .cart-car
+      position: absolute
+      left: 20px
+      top: -15px
+      width: 1rem
+      height: 1rem
+      border-radius: .5rem
+      background: #666
+    .count
+      position: absolute
+      right: 0
+      top: 0
+      width: 1.95rem
+      height: 1.34rem
+      line-height: 1.34rem
+      text-align: center
+      font-size: .5rem
+      background: #00cb00
+      color: #fff
 </style>
